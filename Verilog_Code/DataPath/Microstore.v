8fde60014dd9465e65c0886bfe6adfcf7b3d830e
Brian Rodriguez <brian.rodriguez17@upr.edu>
2019-04-28 14:38:20 -0400

    Implemented ADDIU

--- Verilog_Code/DataPath/Microstore.v
+++ Verilog_Code/DataPath/Microstore.v
@@ -27,6 +27,8 @@ always @ (currentState, reset)
                 7'd15: currentStateSignals = 45'b000000000100001110000000000000000011110100111;
                 7'd16: currentStateSignals = 45'b000110010010000000000000000000000000000100001;
                 7'd17: currentStateSignals = 45'b000110100000000000000000000000100000000100001;
+                7'd18: currentStateSignals = 45'b000111010001000000000000000000000000000100001;
+
 
 
                 // Default state is Reset (0) state.
